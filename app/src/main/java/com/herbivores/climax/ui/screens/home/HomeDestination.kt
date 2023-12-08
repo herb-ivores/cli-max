dcd0fd05b7d6f994e992e8fb469471ab7c0f0bf4
hamthelegend <hamuelxd@gmail.com>
2023-12-08 22:00:17 +0800

    Implemented first UI iteration

--- app/src/main/java/com/herbivores/climax/ui/screens/home/HomeDestination.kt
+++ app/src/main/java/com/herbivores/climax/ui/screens/home/HomeDestination.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.herbivores.climax.apiclient.ApiState
+import com.herbivores.climax.constants.WeatherApi
+import com.herbivores.climax.models.celsius
+import com.herbivores.climax.models.domain.DayWeather
 import com.herbivores.climax.repositories.WeatherRepository
 import com.herbivores.climax.ui.viewmodels.HomeViewModel
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootNavGraph
 
 //@RootNavGraph(start = true)
 //@Destination
@@ -16,7 +18,47 @@ fun Home() {
     val viewModel = viewModel { HomeViewModel(WeatherRepository()) }
 
     val currentWeatherState by viewModel.state.collectAsState()
-//    HomeScreen(
-//        currentWeatherState = currentWeatherState
-//    )
+    HomeScreen(
+        currentWeatherState = currentWeatherState,
+        // Temporary hardcoded values
+        dayWeathersState = ApiState.Success(
+            listOf(
+                DayWeather(
+                    day = "Wednesday",
+                    iconUrl = "${WeatherApi.IMAGE_BASE_URL}01d@2x.png",
+                    type = "Clear",
+                    temperature = 30.celsius,
+                    feelsLike = 32.celsius,
+                ),
+                DayWeather(
+                    day = "Thursday",
+                    iconUrl = "${WeatherApi.IMAGE_BASE_URL}01d@2x.png",
+                    type = "Clear",
+                    temperature = 30.celsius,
+                    feelsLike = 32.celsius,
+                ),
+                DayWeather(
+                    day = "Friday",
+                    iconUrl = "${WeatherApi.IMAGE_BASE_URL}01d@2x.png",
+                    type = "Clear",
+                    temperature = 30.celsius,
+                    feelsLike = 32.celsius,
+                ),
+                DayWeather(
+                    day = "Saturday",
+                    iconUrl = "${WeatherApi.IMAGE_BASE_URL}01d@2x.png",
+                    type = "Clear",
+                    temperature = 30.celsius,
+                    feelsLike = 32.celsius,
+                ),
+                DayWeather(
+                    day = "Sunday",
+                    iconUrl = "${WeatherApi.IMAGE_BASE_URL}01d@2x.png",
+                    type = "Clear",
+                    temperature = 30.celsius,
+                    feelsLike = 32.celsius,
+                ),
+            )
+        ),
+    )
 }
--- app/src/main/java/com/herbivores/climax/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/herbivores/climax/ui/screens/home/HomeScreen.kt
@@ -7,6 +7,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewDynamicColors
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import com.herbivores.climax.apiclient.ApiState
 import com.herbivores.climax.constants.WeatherApi
@@ -26,7 +30,7 @@ fun HomeScreen(
         val dayWeathers = dayWeathersState.data
         
         Scaffold(modifier = modifier) { contentPadding ->
-            LazyColumn(modifier = Modifier.padding(contentPadding)) {
+            LazyColumn(contentPadding = contentPadding) {
                 item {
                     CurrentWeatherHeader(
                         currentWeather = currentWeather,
@@ -44,6 +48,11 @@ fun HomeScreen(
         }
     }
 }
+
+@PreviewScreenSizes
+@PreviewFontScale
+@PreviewLightDark
+@PreviewDynamicColors
 @Preview
 @Composable
 fun HomeScreenPreview() {
@@ -97,6 +106,6 @@ fun HomeScreenPreview() {
                     feelsLike = 32.celsius,
                 ),
             )
-        )
+        ),
     )
 }
