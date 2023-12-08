061332ba521a7f46b8d27808cc84ccdac1b9c427
hamthelegend <hamuelxd@gmail.com>
2023-12-08 22:22:44 +0800

    Removed API key (disabled anyway)

--- app/src/main/java/com/herbivores/climax/constants/WeatherApi.kt
+++ app/src/main/java/com/herbivores/climax/constants/WeatherApi.kt
@@ -2,7 +2,7 @@ package com.herbivores.climax.constants
 
 object WeatherApi {
     const val BASE_URL = "https://api.openweathermap.org"
-    const val KEY = "Insert API Key Here"
+    const val KEY = ApiKeys.WeatherApi
     const val CURRENT_WEATHER = "/data/2.5/weather"
     const val IMAGE_BASE_URL = "https://openweathermap.org/img/wn/"
 }
